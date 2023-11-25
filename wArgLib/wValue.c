ed3d464c2e22e4eeec71aa7b05ed000e537f9ea1
Kai D. Gonzalez <gkai70263@gmail.com>
2023-11-25 14:49:40 -0500

    fix formatting

--- wArgLib/wValue.c
+++ wArgLib/wValue.c
@@ -1,7 +1,7 @@
 // $Id: wValue.c
 #include "wValue.h"
-#include "wError.h"
 #include "wDef.h"
+#include "wError.h"
 
 #include <ctype.h>
 #include <string.h>
@@ -9,14 +9,14 @@
 struct wValue
 {
   wMemPool *_pool; // memory pool
-  int size; // strictly for lists
+  int size;        // strictly for lists
   int capacity;
   union
   {
     char *str;
     int number;
     _Bool boolean;
-    char** lstrs;
+    char **lstrs;
   };
 
   wType type;
@@ -27,14 +27,16 @@ wValueCreate (wMemPool *opt)
 {
   wValue *self = (wValue *)wMemAlloc (opt, sizeof (wValue));
   self->_pool = opt;
-  self->lstrs = (char **)wMemAlloc (opt, sizeof (char *) * WARGLIB_DEFAULT_POOL_INCREMENT);
+  self->lstrs = (char **)wMemAlloc (opt, sizeof (char *)
+                                             * WARGLIB_DEFAULT_POOL_INCREMENT);
   self->capacity = WARGLIB_DEFAULT_POOL_INCREMENT;
   self->size = 0;
 
-  if (!self) { 
-    wErrorDisplay ("wValueCreate: out of memory");
-    return NULL;
-  }
+  if (!self)
+    {
+      wErrorDisplay ("wValueCreate: out of memory");
+      return NULL;
+    }
 
   return self;
 }
@@ -63,10 +65,11 @@ wValueSetBoolean (wValue *self, _Bool boolean)
 void
 wValueSetList (wValue *self)
 {
-  if (!self) {
-    wErrorDisplay("wValueSetList: self is null?\n");
-    return;
-  }
+  if (!self)
+    {
+      wErrorDisplay ("wValueSetList: self is null?\n");
+      return;
+    }
   self->type = WTYPE_LIST;
   self->size = 0;
 }
@@ -74,12 +77,12 @@ wValueSetList (wValue *self)
 void
 wValueAppendList (wValue *self, char *str)
 {
-  if (self->size >= self->capacity) {
-    self->lstrs = realloc (
-        self->lstrs,
-        sizeof (char *) * (self->capacity + WARGLIB_DEFAULT_POOL_INCREMENT)
-    );
-  }
+  if (self->size >= self->capacity)
+    {
+      self->lstrs = realloc (
+          self->lstrs,
+          sizeof (char *) * (self->capacity + WARGLIB_DEFAULT_POOL_INCREMENT));
+    }
   self->lstrs[self->size] = str;
   self->size++;
 }
@@ -93,80 +96,100 @@ wValueList (wValue *self)
 int
 wValueListSize (wValue *self)
 {
-  if (!self || !self->size) {
-    return -1;
-  }
+  if (!self || !self->size)
+    {
+      return -1;
+    }
   return self->size;
 }
 
 char *
 wValueListAt (wValue *self, int index)
 {
-  if (index < 0 || index >= self->size) {
-    return NULL;
-  }
+  if (index < 0 || index >= self->size)
+    {
+      return NULL;
+    }
   return self->lstrs[index];
 }
 
 char *
 wValueStr (wValue *self)
 {
-  if (self && self->type == WTYPE_STRING) {
-    return self->str;
-  }
-  
+  if (self && self->type == WTYPE_STRING)
+    {
+      return self->str;
+    }
+
   return NULL;
 }
 
 int
 wValueNumber (wValue *self)
 {
-  if (self->type == WTYPE_NUMBER) {
-    return self->number;
-  }
+  if (self->type == WTYPE_NUMBER)
+    {
+      return self->number;
+    }
   return -1;
 }
 
 _Bool
 wValueBoolean (wValue *self)
 {
-  if (!self || !self->type) {
-    return false;
-  }
+  if (!self || !self->type)
+    {
+      return false;
+    }
 
-  if (self->type == WTYPE_BOOLEAN) {
-    return self->boolean;
-  }
+  if (self->type == WTYPE_BOOLEAN)
+    {
+      return self->boolean;
+    }
   return false;
 }
 
 wValue *
 wValueFromString (wMemPool *opt, char *str)
 {
-  wValue *value = wValueCreate(opt);
+  wValue *value = wValueCreate (opt);
 
-  for (int i = 0; i < strlen(str); i++) {
-    if (str[i] == '=') {
-      value->str = str + i + 1;
-      value->type = WTYPE_STRING;
-      return value;
+  for (int i = 0; i < strlen (str); i++)
+    {
+      if (str[i] == '=')
+        {
+          wValue* v = wValueFromString (opt, str + i + 1);
+          value = v;
+          return value;
+        }
     }
-  }
 
-  if (isdigit(str[0])) {
-    value->number = strtoul(str, NULL, 10);
-    value->type = WTYPE_NUMBER;
-  }
+  if (str[0] == '0')
+    {
+      if (str[1] == 'x' || str[1] == 'X')
+        {
+          value->number = strtoul (str + 2, NULL, 16);
+          value->type = WTYPE_NUMBER;
+        }
+    }
 
-  else if (strcmp(str, "true") == 0 || strcmp(str, "false") == 0) {
-    value->boolean = (strcmp(str, "true") == 0) ? true : false;
-    value->type = WTYPE_BOOLEAN;
-  }
+  else if (isdigit (str[0]))
+    {
+      value->number = strtoul (str, NULL, 10);
+      value->type = WTYPE_NUMBER;
+    }
 
-  else {
-    value->str = str;
-    value->type = WTYPE_STRING;
-  }
+  else if (strcmp (str, "true") == 0 || strcmp (str, "false") == 0)
+    {
+      value->boolean = (strcmp (str, "true") == 0) ? true : false;
+      value->type = WTYPE_BOOLEAN;
+    }
+
+  else
+    {
+      value->str = str;
+      value->type = WTYPE_STRING;
+    }
 
   return value;
 }
@@ -174,9 +197,10 @@ wValueFromString (wMemPool *opt, char *str)
 wType
 wValueType (wValue *self)
 {
-  if (!self || !self->type) {
-    return WTYPE_UNKNOWN;
-  }
+  if (!self || !self->type)
+    {
+      return WTYPE_UNKNOWN;
+    }
 
   return self->type;
 }
